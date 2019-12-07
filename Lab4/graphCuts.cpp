37b3f5a5c7cc064ef204c466cddf0e64325c3e45
Hichem Bouchaib <hichem.bouchaib@gmail.com>
2019-12-07 03:26:38 +0100

    Graph Cuts

---
+++ Lab4/graphCuts.cpp
@@ -0,0 +1,51 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <fstream>
+
+#include "maxflow/graph.h"
+
+#include "image.h"
+
+using namespace std;
+using namespace cv;
+
+// This section shows how to use the library to compute a minimum cut on the following graph:
+//
+//		        SOURCE
+//		       /       \
+//		     1/         \6
+//		     /      4    \
+//		   node0 -----> node1
+//		     |   <-----   |
+//		     |      3     |
+//		     \            /
+//		     5\          /1
+//		       \        /
+//		          SINK
+//
+///////////////////////////////////////////////////
+
+void testGCuts()
+{
+	Graph<int,int,int> g(/*estimated # of nodes*/ 2, /*estimated # of edges*/ 1); 
+	g.add_node(2); 
+	g.add_tweights( 0,   /* capacities */  1, 5 );
+	g.add_tweights( 1,   /* capacities */  6, 1 );
+	g.add_edge( 0, 1,    /* capacities */  4, 3 );
+	int flow = g.maxflow();
+	cout << "Flow = " << flow << endl;
+	for (int i=0;i<2;i++)
+		if (g.what_segment(i) == Graph<int,int,int>::SOURCE)
+			cout << i << " is in the SOURCE set" << endl;
+		else
+			cout << i << " is in the SINK set" << endl;
+}
+
+int main() {
+	testGCuts();
+
+	Image<Vec3b> Icolor= Image<Vec3b>(imread("../fishes.jpg"));
+	imshow("I",Icolor);
+	waitKey(0);
+	return 0;
+}
